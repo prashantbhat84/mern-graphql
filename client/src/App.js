56d8f3003e8a5d4f60bebaf31ddefacf5c147327
Prashant Bhat <prashantbhat84@gmail.com>
2019-08-13 16:30:51 +0530

    mern with graphql

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,24 @@ import React from 'react';
 import logo from './images/logo.png';
 
 import Launches from './components/Launches';
+import Launch from './components/Launch';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
-    <div className='container'>
-      <img
-        src={logo}
-        alt='SpaceX'
-        style={{ width: 100, height: 50, display: 'block', margin: 'auto' }}
-      />
-      <Launches />
-    </div>
+    <Router>
+      <div className='container'>
+        <img
+          src={logo}
+          alt='SpaceX'
+          style={{ width: 100, height: 50, display: 'block', margin: 'auto' }}
+        />
+        <Route exact path='/' component={Launches} />
+        <Route exact path='/launch/:flight_number' component={Launch} />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import classNames from 'classnames';
+import Moment from 'react-moment';
+import Launch from './Launch';
+import { Link } from 'react-router-dom';
 
 export default function LaunchItem({
   launch: { flight_number, launch_date_local, mission_name, launch_success }
@@ -7,11 +11,26 @@ export default function LaunchItem({
     <div className='card card-body mb-3'>
       <div className='row'>
         <div className='col-md-9'>
-          <h4>Mission:{mission_name}</h4>
-          <p>Date:{launch_date_local}</p>
+          <h4>
+            Mission:
+            <span
+              className={classNames({
+                'text-success': launch_success,
+                'text-danger': !launch_success
+              })}
+            >
+              {mission_name}
+            </span>
+          </h4>
+          <p>
+            Date:
+            <Moment format='DD-MM-YYYY HH:MM:SS'>{launch_date_local}</Moment>
+          </p>
         </div>
         <div className='col-md-3' />
-        <button className='btn btn-secondary'>Launch Details</button>
+        <Link to={`/launch/${flight_number}`} className='btn btn-secondary'>
+          Launch Details
+        </Link>
       </div>
     </div>
   );
