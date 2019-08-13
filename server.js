454a268396648dee88b1ecdbfd9946bd1fce60a7
Prashant Bhat <prashantbhat84@gmail.com>
2019-08-13 17:50:37 +0530

    production code added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,11 @@ app.use(
     graphiql: true
   })
 );
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
